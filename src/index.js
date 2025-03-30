3b906105800ed05f4f1caaac26681a5c39dc08ae
grosiniboss <paultanase1337@gmail.com>
2025-03-30 21:54:51 +0300

    layout mockup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import './style.css';
 
 console.log('Welcome to your Webpack template!');
-
-// LINT TEST
-function sayHi(name) {
-  const age = 10;
-}
-
-const age = 12;
-
-const stuff = ['cool', 'doberman!!!!!!'];
