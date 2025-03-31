ab4391786f52cd699e6bdd94665145a54fb9d87b
grosiniboss <paultanase1337@gmail.com>
2025-03-31 23:22:42 +0300

    final build

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ form.addEventListener('submit', async (e) => {
   }
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_KEY}`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${API_KEY}&contentType=json`,
     );
 
     if (!response.ok) {
@@ -26,12 +26,17 @@ form.addEventListener('submit', async (e) => {
     }
 
     const responseJSON = await response.json();
+    // const celsiusTemp = Math.round(
+    //   ((responseJSON.currentConditions.temp - 32) * 5) / 9,
+    // );
     weatherResult.innerHTML = `
-    <h2>${responseJSON.resolvedAddress}</h2>
+    <h1>${responseJSON.resolvedAddress}</h1>
+    <h2>${responseJSON.description}</h2>
     <div class="results">
+
       <div class="weatherResult">
         <i class="fa-solid fa-temperature-full"></i>
-        <p><b>Temperature:</b> ${responseJSON.currentConditions.temp}°F</p>
+        <p><b>Temperature:</b> ${responseJSON.currentConditions.temp}°C</p>
       </div>
       <div class="weatherResult">
         <i class="fa-solid fa-umbrella"></i>
@@ -39,7 +44,7 @@ form.addEventListener('submit', async (e) => {
       </div>
       <div class="weatherResult">
         <i class="fa-solid fa-cloud-showers-heavy"></i>
-        <p><b>Precipitation:</b> ${responseJSON.currentConditions.precip}%</p>
+        <p><b>Precipitation:</b> ${responseJSON.currentConditions.precip} mm</p>
       </div>
       <div class="weatherResult">
         <i class="fa-solid fa-droplet"></i>
@@ -47,13 +52,30 @@ form.addEventListener('submit', async (e) => {
       </div>
       <div class="weatherResult">
         <i class="fa-solid fa-wind"></i>
-        <p><b>Wind:</b> ${responseJSON.currentConditions.windspeed} m/s</p>
+        <p><b>Wind:</b> ${responseJSON.currentConditions.windspeed} KM/H</p>
       </div>
-
+      <div class="weatherResult">
+        <i class="fa-solid fa-eye"></i>
+        <p><b>Visibility:</b> ${responseJSON.currentConditions.visibility} KM</p>
+      </div>
+      <div class="weatherResult">
+        <i class="fa-solid fa-water"></i> <!--yes I know, I just couldn't find a free bar/pressure icon -->
+        <p><b>Pressure:</b> ${responseJSON.currentConditions.pressure} Millibars</p>
+      </div>
+      <div class="weatherResult">
+        <i class="fa-solid fa-sun"></i>
+        <p><b>Solar Radiation:</b> ${responseJSON.currentConditions.solarradiation} W/m²</p>
+      </div>
+      <div class="weatherResult">
+        <i class="fa-solid fa-poo-storm"></i>
+        <p><b>Solar Energy:</b> ${responseJSON.currentConditions.solarenergy} MJ/m²</p>
+      </div>
+      
     </div>
     `;
     console.log(responseJSON);
   } catch (error) {
+    weatherResult.innerHTML = `<p>Error: ${error.message}`;
     console.error(error);
   }
 });
