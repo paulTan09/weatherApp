4fa83a09a416384a089aea2e849c31bff3c3a13d
grosiniboss <paultanase1337@gmail.com>
2025-03-31 13:35:04 +0300

    more stuff

--- src/index.js
+++ src/index.js
@@ -4,18 +4,34 @@ import './style.css';
 console.log('Welcome to your Webpack template!');
 // vars
 const img = document.querySelector('.image');
+/*
+// Clock/Time API
 
-// Image GIF API
+// GIF API
+async function getGIF() {
+  try {
+    const response = await fetch(
+      'https://api.giphy.com/v1/gifs/translate?api_key=7XadWpnoz8SX6AYb43bhCxFKCsqqPkZg&s=bill-gates',
+    );
+
+    if (!response.ok) {
+      throw new Error("Couldn't fetch GIF");
+    }
+    const responseJSON = await response.json();
+    img.src = responseJSON.data.images.original.url;
+  } catch (error) {
+    console.error(error);
+  }
+}
+getGIF();
+// General Weather API
 fetch(
-  // 'https://api.giphy.com/v1/gifs/translate?api_key=7XadWpnoz8SX6AYb43bhCxFKCsqqPkZg&s=city',
+  'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=UV7ZGWGME2ER8YC5FULQYWCBQ',
   {
     mode: 'cors',
   },
 )
   .then((response) => response.json())
-  .then((response) => {
-    console.log(response.data.images.original.url);
-    img.src = response.data.images.original.url;
-  });
-
-// General Data API
+  .then((data) => console.log(data.currentConditions.datetime))
+  .catch((error) => console.error(error));
+*/
