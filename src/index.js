1a916c7d0df3f30b1a6a9801788642b02afa5170
grosiniboss <paultanase1337@gmail.com>
2025-03-31 15:01:48 +0300

    last commit before redesign

--- src/index.js
+++ src/index.js
@@ -4,14 +4,27 @@ import './style.css';
 console.log('Welcome to your Webpack template!');
 // vars
 const img = document.querySelector('.image');
-/*
-// Clock/Time API
+const date = document.querySelector('.date');
+
+// Clock/Time API (couldn't find a free one that queries by city so I'm resorting to system time)
+function updateClock() {
+  const now = new Date();
+
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+
+  document.querySelector('.clock').textContent =
+    `${hours}:${minutes}:${seconds}`;
+}
+updateClock();
+setInterval(updateClock, 1000);
 
 // GIF API
 async function getGIF() {
   try {
     const response = await fetch(
-      'https://api.giphy.com/v1/gifs/translate?api_key=7XadWpnoz8SX6AYb43bhCxFKCsqqPkZg&s=bill-gates',
+      'https://api.giphy.com/v1/gifs/translate?api_key=7XadWpnoz8SX6AYb43bhCxFKCsqqPkZg&s=cat',
     );
 
     if (!response.ok) {
@@ -24,14 +37,22 @@ async function getGIF() {
   }
 }
 getGIF();
+
 // General Weather API
-fetch(
-  'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=UV7ZGWGME2ER8YC5FULQYWCBQ',
-  {
-    mode: 'cors',
-  },
-)
-  .then((response) => response.json())
-  .then((data) => console.log(data.currentConditions.datetime))
-  .catch((error) => console.error(error));
-*/
+async function getWeather() {
+  try {
+    const response = await fetch(
+      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=UV7ZGWGME2ER8YC5FULQYWCBQ',
+    );
+
+    if (!response.ok) {
+      throw new Error("Couldn't fetch Weather data");
+    }
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+    date.textContent = responseJSON.days[0].datetime;
+  } catch (error) {
+    console.error(error);
+  }
+}
+getWeather();
