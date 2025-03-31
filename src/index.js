69bfdde7f61225abb77954bbfa30d54fbb33b379
grosiniboss <paultanase1337@gmail.com>
2025-03-31 10:46:56 +0300

    api test (works)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+import './template.html';
 import './style.css';
 
 console.log('Welcome to your Webpack template!');
+// vars
+const img = document.querySelector('.image');
+
+// Image GIF API
+fetch(
+  // 'https://api.giphy.com/v1/gifs/translate?api_key=7XadWpnoz8SX6AYb43bhCxFKCsqqPkZg&s=city',
+  {
+    mode: 'cors',
+  },
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response.data.images.original.url);
+    img.src = response.data.images.original.url;
+  });
+
+// General Data API
